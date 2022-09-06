1f561baac666aadf77076f79dafcbf89cc7d4d4c
dhanushiya06 <dhanushiya510@gmail.com>
2022-09-06 22:24:04 +0530

    day 20

--- day19/script.js
+++ day19/script.js
@@ -2,8 +2,9 @@ const compute = () => {
   let amount = document.getElementById("amount").value;
   let interestRate = document.getElementById("interest").value;
   let months = document.getElementById("month").value;
+  
 let interest = (amount*(interestRate*0.1))/months;
 let payment = (amount/months + interest).toFixed(2);
 
-document.getElementById("payment").innerHTML = "Monthly payment = Rs. " +payment;
+document.getElementById("payment").innerHTML = "Monthly payment : Rs. " +payment;
 };
